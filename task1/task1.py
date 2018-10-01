85db6e0892b0f8adc39409984f66c062da88f519
ivan.tishchenko <ivan.tishchenko96@gmail.com>
2018-10-01 17:30:34 +0200

    Implemented complettion by column means

--- task1/task1.py
+++ task1/task1.py
@@ -2,12 +2,16 @@ import csv
 import numpy as np
 from sklearn import linear_model
 
-
 TRAIN_M = 1212
 TRAIN_N = 887
 
 TEST_M = 776
 TEST_N = 887
+
+'''
+Load the test data
+'''
+
 
 def __load_test_data():
     # Load X_test
@@ -25,8 +29,13 @@ def __load_test_data():
                     feature_list.append(float(row['x' + str(i)]))
             feature_string_matrix.append(feature_list)
         X_test = np.array(feature_string_matrix)
-
         return X_test
+
+
+'''
+Load the train data
+'''
+
 
 def __load_train_data():
     # Load X_train
@@ -52,9 +61,34 @@ def __load_train_data():
             y_value = [float(row['y'])]
             y_string.append(y_value)
         y_train = np.array(y_string)
-
     return X_train, y_train
 
+
+'''
+Complete missing values in the data matrix
+'''
+
+
+def __complete_matrix(X):
+    # get col means
+    col_mean = np.nanmean(X, axis=0)
+    # Find indicies that you need to replace
+    idxs = np.where(np.isnan(X))
+    X[idxs] = np.take(col_mean, idxs[1])
+
+
+# Load data
 X_train, y_train = __load_train_data()
 X_test = __load_test_data()
 
+# Do data processing
+
+# Replace NaNs by the colum means
+__complete_matrix(X_train)
+__complete_matrix(X_test)
+
+# Do regression
+reg = linear_model.LinearRegression()
+reg.fit(X_train, y_train)
+y_test = reg.predict(X_test)
+pass
